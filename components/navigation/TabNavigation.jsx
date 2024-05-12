98bce4416c2c76511f9ff2c1fda0bef13310013f
maeldcls <maelduclos03@gmail.com>
2024-05-13 00:11:00 +0200

    HomeScreen Header

--- components/navigation/TabNavigation.jsx
+++ components/navigation/TabNavigation.jsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../Screens/HomeScreen';
-import ProfileScreen from '../Screens/ProfileScreen';
-import BookingScreen from '../Screens/BookingScreen';
+import HomeScreen from '../Screens/HomeScreen/HomeScreen';
+import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
+import BookingScreen from '../Screens/BookingScreen/BookingScreen';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { Entypo } from '@expo/vector-icons';
 import Colors from '@/app/utils/Colors';
